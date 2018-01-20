1c5655960875bf1b919c9fefcf84111f86ccd536
kiddr1 <kiddr@flip1.engr.oregonstate.edu>
2018-01-19 21:15:38 -0800

    Refactored and bugs additions

--- projects/kiddr/dominion/dominion.c
+++ projects/kiddr/dominion/dominion.c
@@ -1329,5 +1329,124 @@ int updateCoins(int player, struct gameState *state, int bonus)
 }
 
 
+//Bug 1: Smithy
+int playSmithy(struct gameState *state, int handPos)
+{
+  int i;
+  int currentPlayer = whoseTurn(state);
+
+  //+3 Cards
+  //for (i = 0; i < 3; i++) //*ORIGINAL CODE*
+  for (i = 0; i <= 3; i++) //*BUG* - incorrect logical operator
+  {
+    drawCard(currentPlayer, state);
+  }
+
+  //discard card from hand
+  discardCard(handPos, currentPlayer, state, 0);
+  return 0;
+}
+
+//Bug 2: Adventurer
+int playAdventurer(struct gameState *state)
+{
+  int currentPlayer = whoseTurn(state);
+  int temphand[MAX_HAND];// moved above the if statement
+  int drawntreasure=0;
+  int cardDrawn;
+  int z = 0;
+
+  while(drawntreasure<2)
+  {
+    if (state->deckCount[currentPlayer] <1)//if the deck is empty we need to shuffle discard and add to deck
+    {
+    shuffle(currentPlayer, state);
+    }
+    drawCard(currentPlayer, state);
+    cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+    //if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold) // Bug fixed *ORIGINAL CODE*
+    if (cardDrawn == copper || cardDrawn == gold) //*BUG* - Silver deleted
+      drawntreasure++;
+    else
+    {
+      temphand[z]=cardDrawn;
+      state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+      z++;
+    }
+  }
+
+  while(z-1>=0)
+  {
+    state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+    z=z-1;
+  }
+  return 0;
+}
+
+//Bug 3: Council Room
+int playCouncil_Room(struct gameState *state, int handPos)
+{
+  int i;
+  int currentPlayer = whoseTurn(state);
+
+  //+4 Cards
+  for (i = 0; i < 4; i++)
+  {
+    drawCard(currentPlayer, state);
+  }
+
+  //Bug 4: Great Hall
+  int playGreat_Hall(struct gameState *state, int handPos)
+  {
+    int currentPlayer = whoseTurn(state);
+
+    //+1 Card
+    drawCard(currentPlayer, state);
+
+    //+1 Actions
+    state->numActions++; //BUG FIXED *ORIGINAL CODE*
+    //state->numBuys++; //*BUG INTRODUCED* - increment wrong variable
+
+    //discard card from hand
+    discardCard(handPos, currentPlayer, state, 0);
+    return 0;
+  }
+
+  int playVillage(struct gameState *state, int handPos) //No BUG
+  {
+    int currentPlayer = whoseTurn(state);
+
+    //+1 Card
+    drawCard(currentPlayer, state);
+
+    //+2 Actions
+    state->numActions = state->numActions + 2;
+
+    //discard played card from hand
+    discardCard(handPos, currentPlayer, state, 0);
+    return 0;
+  }
+
+
+  //+1 Buy
+  state->numBuys++;
+
+  //Each other player draws a card
+  for (i = 0; i < state->numPlayers; i++)
+  {
+    if ( i != currentPlayer ) // bug fixed *ORIGINAL CODE*
+    //if ( i == currentPlayer ) //*BUG INTRODUCED* - Wrong logical operator
+    {
+      drawCard(i, state);
+    }
+  }
+
+  //put played card in played card pile
+  discardCard(handPos, currentPlayer, state, 0);
+
+  return 0;
+}
+
+
 //end of dominion.c
 
